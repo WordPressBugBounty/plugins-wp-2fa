536f48beeb6a69fea81d061ec2ae91e388b2b69d
Sean Murphy <sean@iamseanmurphy.com>
2026-02-06 03:35:10 +0000

    3.1.0

--- wp-2fa/includes/classes/Shortcodes/class-shortcodes.php
+++ wp-2fa/includes/classes/Shortcodes/class-shortcodes.php
@@ -42,7 +42,7 @@ if ( ! class_exists( '\WP2FA\Shortcodes\Shortcodes' ) ) {
 		 */
 		public static function register_2fa_shortcode_scripts() {
 			// Add our front end stuff, which we only want to load when the shortcode is present.
-			\wp_register_script( 'wp_2fa_frontend_scripts', Core\script_url( 'wp-2fa', 'admin' ), array( 'jquery', 'wp_2fa_micro_modals' ), WP_2FA_VERSION, true );
+			\wp_register_script( 'wp_2fa_frontend_scripts', Core\script_url( 'wp-2fa', 'admin' ), array( 'jquery', 'wp_2fa_micro_modals', 'wp-i18n' ), WP_2FA_VERSION, true );
 			\wp_register_script( 'wp_2fa_micro_modals', Core\script_url( 'micromodal', 'admin' ), array(), WP_2FA_VERSION, true );
 			\wp_register_style( 'wp_2fa_styles', Core\style_url( 'styles', 'frontend' ), array(), WP_2FA_VERSION );
 
@@ -172,7 +172,7 @@ if ( ! class_exists( '\WP2FA\Shortcodes\Shortcodes' ) ) {
 				$atts
 			);
 
-			$configure_2fa_url = sanitize_text_field( $atts['configure_2fa_url'] );
+			$configure_2fa_url = \esc_url_raw( $atts['configure_2fa_url'] );
 
 			// TODO: is that really necessary?
 			User_Notices::init();
@@ -192,6 +192,7 @@ if ( ! class_exists( '\WP2FA\Shortcodes\Shortcodes' ) ) {
 					'allDoneHeading' => \esc_html__( 'All done.', 'wp-2fa' ),
 					'allDoneText'    => \esc_html__( 'Your login just got more secure.', 'wp-2fa' ),
 					'closeWizard'    => \esc_html__( 'Close Wizard', 'wp-2fa' ),
+					'redirectToUrl'  => $configure_2fa_url,
 				);
 				\wp_localize_script( 'wp_2fa_frontend_scripts', 'wp2faData', $data_array );
 
